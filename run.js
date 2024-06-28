b87d552aa6f75599dfc7f62b1f5f91b93eefbf2a
jujutstukaisen9 <146422990+jujutstukaisen9@users.noreply.github.com>
2024-06-28 13:59:19 +0530

    Update run.js

--- run.js
+++ run.js
@@ -2,19 +2,15 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 const winston = require('winston');
 
-// Set up logging
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
     winston.format.timestamp(),
     winston.format.printf(({ timestamp, level, message }) => `${timestamp} - ${level.toUpperCase()} - ${message}`)
   ),
-  transports: [
-    new winston.transports.Console()
-  ]
+  transports: [new winston.transports.Console()]
 });
 
-// Constants
 const API_URL = "https://tplayapi.code-crafters.app/321codecrafters/fetcher.json";
 const HMAC_URL = "https://tplayapi.code-crafters.app/321codecrafters/hmac.json";
 const RETRIES = 3;
@@ -29,9 +25,7 @@ async function fetchApi(url, retries) {
       return await response.json();
     } catch (error) {
       logger.error(`Error fetching API data (attempt ${attempt + 1}/${retries}): ${error.message}`);
-      if (attempt < retries - 1) {
-        continue;
-      } else {
+      if (attempt === retries - 1) {
         return null;
       }
     }
@@ -42,11 +36,10 @@ function generateM3U8(data, hmac) {
   let m3u8Content = `#EXTM3U x-tvg-url="https://raw.githubusercontent.com/mitthu786/tvepg/main/tataplay/epg.xml.gz"\n\n`;
 
   for (const channel of data.channels) {
-    if (channel.clearkeys && channel.clearkeys.length > 0) {
-      const clearkey = channel.clearkeys[0].base64;
+    const clearkeys = channel.clearkeys;
+    if (clearkeys && clearkeys.length > 0) {
+      const clearkey = clearkeys[0].base64;
       const userAgent = channel.manifest_headers['User-Agent'];
-
-      // Use the correct 'hdntl' key
       const cookie = hmac.hmac.hdntl.value;
 
       m3u8Content += `#EXTINF:-1 tvg-id="${channel.id}" group-title="${channel.genres.join(', ')}", tvg-logo="${channel.logo_url}", ${channel.name}\n`;
@@ -68,7 +61,7 @@ async function main() {
   if (fetcherData && hmacData) {
     const m3u8Content = generateM3U8(fetcherData.data, hmacData.data);
     fs.writeFileSync('ts.m3u', m3u8Content);
-    logger.info("M3U8 playlist generated and saved to playlist.m3u8");
+    logger.info("M3U8 playlist generated and saved to ts.m3u");
   } else {
     logger.error("Failed to fetch data from API");
   }
